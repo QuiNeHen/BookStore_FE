b44029250b12110705a7877bc63e01430604bde6
QuiNeHen <160574473+QuiNeHen@users.noreply.github.com>
2024-04-09 09:35:33 +0700

    FE du an

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <script>
+import AppHeader from '@/components/AppHeader.vue';
+// import InputSearch from '@/components/InputSearch.vue';
+// import ContactList from '@/components/ContactList.vue';
+
 export default {
-}
+  components: {
+    AppHeader,
+    // InputSearch,
+    // ContactList
+  },
+};
 </script>
+
 <template>
-  <h1>Hello, Vue.js!</h1>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
+
 <style>
-.page {
+.container {
   max-width: 400px;
   margin: auto;
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import './assets/main.css'
+import { createApp } from "vue";
+// import Vue from 'vue';
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import router from "./router";
+createApp(App).use(router).mount("#app");
+// Import các component mới
+// import AppHeader from './components/AppHeader.vue';
+// import InputSearch from './components/InputSearch.vue';
+// import ContactList from './components/ContactList.vue';
 
-createApp(App).mount('#app')
+// // Vue.config.productionTip = false;
+
+// // Đăng ký các component mới
+// Vue.component('AppHeader', AppHeader);
+// Vue.component('InputSearch', InputSearch);
+// Vue.component('ContactList', ContactList);
+
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app');
